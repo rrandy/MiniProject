030d71b362e48f74e71e8c17c49896167472ad3e
Carraram <marine.carrara@gmail.com>
2015-01-20 17:39:37 +0100

    Parseur XML fonctionnel

--- app/src/main/java/com/m2dl/miniproject/mini_projetandroid/util/InterestXmlParser.java
+++ app/src/main/java/com/m2dl/miniproject/mini_projetandroid/util/InterestXmlParser.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
@@ -56,14 +57,12 @@ public class InterestXmlParser {
     private void fillElements() {
         NodeList allElements = document.getElementsByTagName("element");
         String attributeName;
-        String nodeTextValue;
+        String elementName;
         for (int i = 0; i < allElements.getLength(); i++) {
-            attributeName = allElements.item(i).getAttributes().item(0).getNodeValue();
-            nodeTextValue = getFirstLevelTextContent(allElements.item(i));
-            if (!nodeTextValue.equals("")) {
-                elements.put(attributeName, nodeTextValue);
-                elementIDs.add(attributeName);
-            }
+            attributeName = getAttributeByName(allElements.item(i), "id");
+            elementName = getFirstLevelTextContent(allElements.item(i));
+            elements.put(attributeName, elementName);
+            elementIDs.add(attributeName);
         }
     }
 
@@ -76,22 +75,29 @@ public class InterestXmlParser {
     private List<String> getDescendants(Element e) {
         NodeList childrenList = e.getChildNodes();
         List<String> children = new ArrayList<>();
+        Node currentNode;
         for (int i = 1; i < childrenList.getLength(); i++) {
-            children.add(childrenList.item(i).getNodeName());
+            currentNode = childrenList.item(i);
+            if (currentNode.getNodeType() == Node.ELEMENT_NODE) {
+                children.add(getAttributeByName(childrenList.item(i),"id"));
+            }
         }
 
         return children;
     }
 
     private String getFirstLevelTextContent(Node node) {
-        NodeList list = node.getChildNodes();
-        StringBuilder textContent = new StringBuilder();
-        for (int i = 0; i < list.getLength(); ++i) {
-            Node child = list.item(i);
-            if (child.getNodeType() == Node.TEXT_NODE)
-                textContent.append(child.getTextContent());
+        return node.getChildNodes().item(0).getTextContent().replaceAll("(\\r|\\n)", "");
+    }
+
+    private String getAttributeByName(Node node, String attrName) {
+        NamedNodeMap attributes = node.getAttributes();
+        for (int i = 0; i < attributes.getLength(); i++) {
+            if (attributes.item(i).getNodeName().equals(attrName)) {
+                return attributes.item(i).getNodeValue();
+            }
         }
-        return textContent.toString();
+        return null;
     }
 
     public List<String> getDescendantsForElement(String elementId) {
