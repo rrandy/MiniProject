abbb61fbf4ecec92dfa79512f998c919a9d30fcc
rrandy <randy.ratsim@gmail.com>
2015-01-21 20:19:23 +0100

    Licence GPL

--- app/src/main/java/com/m2dl/miniproject/mini_projetandroid/business/DataStorage.java
+++ app/src/main/java/com/m2dl/miniproject/mini_projetandroid/business/DataStorage.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2015 Marine Carrara, Akana Mao, Randy Ratsimbazafy
+ *
+ * This file is part of Biodiversity.
+ *
+ * Biodiversity is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Biodiversity is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Biodiversity.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.m2dl.miniproject.mini_projetandroid.business;
 
 import android.content.Context;
